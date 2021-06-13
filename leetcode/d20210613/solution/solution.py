f5c10ed83efd599c8ba9dda88af88b662d94cf57
Mohamed Anis MANI <manimohamed@gmail.com>
2021-06-13 09:51:42 +0100

    Added 13th June Solution, TLE

--- leetcode/d20210613/solution/solution.py
+++ leetcode/d20210613/solution/solution.py
@@ -3,7 +3,26 @@ from typing import List
 
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        pass
+        def isPalindrome(s):
+            i, j = 0, len(s)-1
+            while j > i and s[i] == s[j]:
+                i, j = i+1, j-1
+            return i >= j
+
+        res = []
+        n = len(words)
+        for i in range(n):
+            for j in range(i+1, n):
+                w = words[i] + words[j]
+                w1 = words[j] + words[i]
+                if isPalindrome(w):
+                    res.append([i, j])
+                    if w1 == w:
+                        res.append([j, i])
+                        continue
+                if isPalindrome(w1):
+                    res.append([j, i])
+        return res
 
 
 if __name__ == '__main__':
